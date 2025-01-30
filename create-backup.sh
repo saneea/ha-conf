ebc7735b462d5e30c6fc70bb2f905bcebe13b39d
Alexander Sokol <saneea@bk.ru>
2025-01-30 23:31:57 +0300

    add compose.yaml and .env files to backup

--- create-backup.sh
+++ create-backup.sh
@@ -1,8 +1,14 @@
 tar							\
+							\
 --exclude='data/ha/config/backups'			\
 --exclude='data/ha/config/home-assistant.log*'		\
 --exclude='data/mqtt/log'				\
 --exclude='data/z2m/app/data/log'			\
+							\
 -czvf							\
+							\
 ha-stack-data-$(date '+%F--%H-%M-%S').tar.gz		\
-./data
+							\
+./data							\
+./compose.yaml						\
+./.env
